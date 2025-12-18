e869a07914fd40dae9e328609d0dc9b066ffaf09
Erick de los Reyes <delosreyeserick90@gmail.com>
2025-12-18 01:42:24 +0000

    Version 1 Finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  const pronoun = ["the", "our", "that"];
+  const adjetive = ["great", "big", "wonderful"];
+  const noun = ["rocker", "cat", "dog", "irish"];
+  const domainList = [];
+
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adjetive.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+       const domain = pronoun[i] + adjetive[j] + noun[k] + ".com";
+       domainList.push(domain);
+      }
+     }
+    }
+  //Selected placeholder
+  const alertBox = document.querySelector(".alert");
+  console.log(domainList);
+  alertBox.innerHTML = "";
+  domainList.forEach((domain) => {
+alertBox.innerHTML += domain + "<br>";
+  });
 };
